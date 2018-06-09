9936849db528652173a420adfe28f485dda67f61
javen <javenllee@gmail.com>
2018-06-09 17:03:48 +0800

    init :tada:

---
+++ daenerys-api/src/main/java/run/rua/daenerys/api/DaenerysProperties.java
@@ -0,0 +1,22 @@
+package run.rua.daenerys.api;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:daenerys.properties")
+@ConfigurationProperties(prefix = "daenerys")
+public class DaenerysProperties {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- daenerys-api/src/main/java/run/rua/daenerys/api/controller/HomeController.java
+++ daenerys-api/src/main/java/run/rua/daenerys/api/controller/HomeController.java
@@ -1,18 +1,40 @@
 package run.rua.daenerys.api.controller;
 
 
-import com.fasterxml.jackson.annotation.JsonSetter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import run.rua.daenerys.api.DaenerysProperties;
 
 @RestController
 @RequestMapping(value = "api",method = RequestMethod.POST)
 public class HomeController {
-    public Object Get(Object req)
-    {
-        return req;
+
+
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    private DaenerysProperties daenerysProperties;
+
+    @Autowired
+    public HomeController(DaenerysProperties customProperties) {
+        this.daenerysProperties = customProperties;
     }
 
+    @RequestMapping(value = "properties/daenerys",method = RequestMethod.GET)
+    public Object GetProperties()
+    {
+        log.info("load..");
+        return daenerysProperties;
+    }
+
+
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    public Object Get()
+    {
+        return "hello world";
+    }
 }
